03d1611eace77628e906b0dfa96dc838ab628f8d
HZilla666 <hugocartoux@gmail.com>
2025-12-23 15:48:52 +0100

    supppression d'une formation

---
+++ src/Controller/admin/AdminFormationsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminFormationsController
+ *
+ * @author hugoc
+ */
+class AdminFormationsController extends AbstractController{
+    /**
+     *
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     *
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+
+    private const VIEW = "admin/admin.formations.html.twig";
+    
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+    
+    #[Route('/admin/formations', name: 'admin.formations')]
+    public function index(): Response{
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::VIEW, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/admin/formations/suppr/{id}', name: 'admin.formations.suppr')]
+    public function suppr($id): Response{
+        $formation = $this->formationRepository->find($id);
+        $this->formationRepository->remove($formation);
+        return $this->redirectToRoute('admin.formations');
+    }
+}
